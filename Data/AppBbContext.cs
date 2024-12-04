4d3833f999780608b9660491e6030d2c6cf9d9d7
Mustafa Shah <shahmustafa051@gmail.com>
2024-12-04 07:34:35 +0100

    Add project files.

---
+++ Data/AppBbContext.cs
@@ -0,0 +1,22 @@
+﻿using BlazorDB.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorDB.Data
+{
+    public class AppBbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public AppBbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DbConnectionstring"));
+        }
+
+        public DbSet<DataDB> DataDB { get; set; }
+    }
+}
---
+++ Model/DataDB.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorDB.Model
+{
+    public class DataDB
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int PhoneNum { get; set; }
+
+        public string Email  { get; set; }
+    }
+}
